0536e633e76429b323dd5e4108a4ca8351741a05
Carvz9 <crvz0903@gmail.com>
2026-06-29 14:31:46 +0800

    Set up VisionAI project files

---
+++ app/camera.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function CameraPage() {
+  return (
+    <View>
+      <Text>Camera Screen</Text>
+    </View>
+  );
+}
---
+++ app/result.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function ResultPage() {
+  return (
+    <View>
+      <Text>Result Screen</Text>
+    </View>
+  );
+}
